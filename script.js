766511bd63d360a5f5e70da15732bbb1c252abc2
jameswim <james384712@gmail.com>
2024-09-06 22:04:19 +0800

    Add collapsible YouTube submenu in sidenav

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function toggleYoutube() {
+    var submenu = document.getElementById("youtubeSubmenu");
+    if (submenu.style.display === "none") {
+        submenu.style.display = "block";
+    } else {
+        submenu.style.display = "none";
+    }
+}
